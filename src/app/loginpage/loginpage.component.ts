a9b68594b7bc29f51e1a310d61b35a990766ae2a
Gayathri <ggnanasekaran@simplesolve.com>
2025-07-17 12:19:32 +0530

    Initial commit

---
+++ src/app/loginpage/loginpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginpage',
+  standalone: false,
+  templateUrl: './loginpage.component.html',
+  styleUrl: './loginpage.component.css'
+})
+export class LoginpageComponent {
+   
+}
