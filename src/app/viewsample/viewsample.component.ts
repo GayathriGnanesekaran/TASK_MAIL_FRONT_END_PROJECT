040de9830554e2912b0d4bbb8a50c461c0326368
Gayathri <ggnanasekaran@simplesolve.com>
2025-07-29 11:23:13 +0530

    added style.scss

---
+++ src/app/viewsample/viewsample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewsample',
+  standalone: false,
+  templateUrl: './viewsample.component.html',
+  styleUrl: './viewsample.component.css'
+})
+export class ViewsampleComponent {
+
+}
